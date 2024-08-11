e99e82a09f8a0a61172bb7ce1ec72e2d8cc749db
Mickhail Fedoseev <fedoseevmvf@yandex.ru>
2024-08-11 10:46:35 +0300

    check process in Yandex

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -4,11 +4,12 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Category, Location, Post
 from django.utils import timezone
 
+
 def index(request):
     template_name = 'blog/index.html'
     post_list = Post.objects.filter(
         pub_date__lt=timezone.now(), is_published=True, category__is_published=True
-    ).select_related('author', 'location', 'category')
+    ).select_related('author', 'location', 'category')[0:5]
     context = {
         'post_list': post_list
     }
@@ -17,7 +18,10 @@ def index(request):
 
 def post_detail(request, pk):
     template_name = 'blog/detail.html'
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(
+        Post.objects.filter(is_published=True, pub_date__lt=timezone.now(), category__is_published=True),
+        pk=pk
+    )
     context = {
         'post': post,
     }
@@ -26,9 +30,10 @@ def post_detail(request, pk):
 
 def category_posts(request, category_slug):
     template_name = 'blog/category.html'
-    # category = Category.objects.get(slug__exact=category_slug)
-    queryset = Category.objects.filter(slug__exact=category_slug)
-    get_object_or_404(queryset, is_published=True)
+    get_object_or_404(
+        Category.objects.filter(slug__exact=category_slug),
+        is_published=True
+    )
 
     post_list = Post.objects.filter(
         category__slug__exact=category_slug,
